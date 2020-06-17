5b68268cf4ac9625edcfb650a797fa48b4c2fe04
panpanini <panpanini@mercari.com>
2020-06-17 12:56:46 +0900

    use Java style args definition

--- src/main/java/com/vector/svg2vectorandroid/Runner.java
+++ src/main/java/com/vector/svg2vectorandroid/Runner.java
@@ -5,7 +5,7 @@ package com.vector.svg2vectorandroid;
  */
 public class Runner {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         if(args.length == 0){
             System.out.println(" Provide source directory as first argument for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
