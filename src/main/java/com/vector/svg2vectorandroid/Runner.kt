bb3ae788df7f5fa971455a2fc5f5cd43db1219b6
panpanini <panpanini@mercari.com>
2020-06-17 13:22:45 +0900

    add --help handling

--- src/main/java/com/vector/svg2vectorandroid/Runner.kt
+++ src/main/java/com/vector/svg2vectorandroid/Runner.kt
@@ -2,15 +2,19 @@ package com.vector.svg2vectorandroid
 
 
 val usage = """
-    Provide source directory as first argument for svg files to be converted\n
+    Provide source directory as first argument for svg files to be converted 
     example: java -jar Svg2VectorAndroid-1.1.2.jar <SourceDirectoryPath>
 """.trimIndent()
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        println(usage)
-        return
+        return println(usage)
     }
+
+    if (args.contains("--help")) {
+        return println(usage)
+    }
+
     val sourceDirectory = args[0]
     if (sourceDirectory.isNotEmpty()) {
         val processor = SvgFilesProcessor(sourceDirectory)
