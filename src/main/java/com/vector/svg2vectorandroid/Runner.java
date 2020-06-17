d117c1b03494ed3713e6510e68e6810e5846aafc
panpanini <panpanini@mercari.com>
2020-06-17 12:56:30 +0900

    fix spelling

--- src/main/java/com/vector/svg2vectorandroid/Runner.java
+++ src/main/java/com/vector/svg2vectorandroid/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
     public static void main(String args[]){
 
         if(args.length == 0){
-            System.out.println(" Provide source directory as first arguement for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
+            System.out.println(" Provide source directory as first argument for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
             return;
         }
 
